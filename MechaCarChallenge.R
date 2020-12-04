69f82dc946948f5a23ec07abed790c21c9206942
noelpdot <66786205+noelpdot@users.noreply.github.com>
2020-12-04 14:21:02 -0500

    trip analysis updates.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,5 +7,9 @@ head(df)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = df)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = df))
 
-
+#2 VISUALIZATION TRIP ANALYSIS
+table_sc <- read.csv('Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+head(table_sc)
+total_summary <- table_sc %>% summarise(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+lot_summary <- table_sc %>% group_by(Manufacturing_Lot) %>% summarise(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
                                                                       
